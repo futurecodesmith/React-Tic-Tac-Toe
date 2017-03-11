0219fde99236c06c5653431a6a1bb3eae19d9697
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-11 12:28:43 -0800

    add style with functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,77 @@ import './App.css';
     //this.turn = 'X'
     this.state = {
       turn: 'X',
-      gameOver: false
+      boardState: {}
     }
   }
 
-  changeState() {
-    return this.state.turn === 'X' ?
-    this.setState({turn: 'O'}) :
-    this.setState({turn: 'X'})
+  stateAt(id) {
+    return this.state.boardState[id];
+  }
+
+  checkColumn(id) {
+    if (id >= 20) return this.stateAt(id) === this.stateAt(id - 10) && this.stateAt(id - 10) === this.stateAt(id - 20);
+    else if (id >= 10) return this.stateAt(id) === this.stateAt(id - 10) && this.stateAt(id - 10) === this.stateAt(id + 10);
+    else return this.stateAt(id) === this.stateAt(id + 10) && this.stateAt(id + 10) === this.stateAt(id + 20)
+  }
+  
+  checkRow(id) {
+    if (id % 10 === 0) return this.stateAt(id) === this.stateAt(id + 1) && this.stateAt(id + 1) === this.stateAt(id + 2);
+    else if (id % 10 === 1) return this.stateAt(id) === this.stateAt(id - 1) && this.stateAt(id - 1) === this.stateAt(id + 1);
+    else return this.stateAt(id) === this.stateAt(id -1) && this.stateAt(id - 1) === this.stateAt(id - 2)
+  }
+
+  checkDiagonal(id) {
+    if (id === 0 || id === 22) {
+      return this.stateAt(0) === this.stateAt(11) && this.stateAt(11) === this.stateAt(22)
+    } else if (id === 2 || id === 20) {
+      return this.stateAt(2) === this.stateAt(11) && this.stateAt(11) === this.stateAt(20)
+    } else if (id === 11) {
+      if ( (this.stateAt(0) === this.stateAt(11) && this.stateAt(11) === this.stateAt(22)) || (this.stateAt(2) === this.stateAt(11) && this.stateAt(11) === this.stateAt(20)) ) return true;
+    }
+    return false;
+
+
+    // if (id === 0 || id === 11 || id === 22 || id === 2 || id === 20) {
+    //   return this.stateAt(0) === this.stateAt(11) && this.stateAt(11) === this.stateAt(22) ? true : 
+    //         this.stateAt(2) === this.stateAt(11) && this.stateAt(11) === this.stateAt(20) 
+    // } return false; 
+  }
+
+  checkIfGameOver(id) {
+    return this.checkColumn(id) ? true : 
+           this.checkRow(id) ? true : 
+           this.checkDiagonal(id) 
+  }
+
+  changeState(id) {
+    let newBoard = this.state.boardState;
+    if (this.state.turn === 'X') {
+      newBoard[id] = 'O' 
+      this.setState(Object.assign( this.state, { turn: 'O', boardState: newBoard } ));
+    } else {
+       newBoard[id] = 'X' 
+      this.setState(Object.assign( this.state, { turn: 'X', boardState: newBoard } ));
+    }
+      if (this.checkIfGameOver(id)) {
+        this.setState(Object.assign( this.state, {winner: newBoard[id] + ' WINS!!!!'} ))
+
+        }
+  }
+
+  renderWinner() {
+    return (
+      <div className="winner"> {this.state.winner} </div>  
+    )
+
   }
 
   render() {
     return (
-    <div>
+    <div className="container">
       <h1>Tic Tac Toe</h1>
       <Board change={this.changeState.bind(this)} state={this.state} />
+      {this.renderWinner()}
     </div>
     )
   }
@@ -33,8 +89,6 @@ class Board extends App {
     super(state);    
   }
 
-
-
   populateBoard() {
     var rows = [];
     for (let i = 0; i < 3; i += 1) {
@@ -43,7 +97,6 @@ class Board extends App {
     return rows;
   }
 
-  
   render() {
     return (
     <div>
@@ -51,7 +104,6 @@ class Board extends App {
     </div>
     )
   }
-
 
 }
 
@@ -64,7 +116,8 @@ class Row extends Board {
   populateRow() {
     var boxes = [];
     for (let i = 0; i < 3; i += 1) {
-      boxes.push(<Box change={this.props.change} state={this.props.state} key={i} boxVal={{rowKey: this.props.rowKey, boxKey: i}} boxKey={i} rowKey={this.props.rowKey}/>)
+      let id = this.props.rowKey*10+i
+      boxes.push(<Box id={id} change={this.props.change} state={this.props.state} key={i} boxVal={{rowKey: this.props.rowKey, boxKey: i}}/>)
     }
     return boxes;
   }
@@ -76,7 +129,6 @@ class Row extends Board {
     </div>
     )
   }
-
 
 }
 
@@ -93,7 +145,7 @@ class Box extends Row {
       this.props.state.turn === 'X' ?
       this.icon = 'O' :
       this.icon = 'X' 
-      this.props.change()
+      this.props.change(this.props.id);
     }
     //checkIfGameOver()
    /* 
@@ -105,19 +157,18 @@ class Box extends Row {
   */
   }
 
-  checkIfGameOver() {
-    
-  }
+ 
 
   render() {
     // console.log('ROW:', this.props.rowKey, 'BOX:', this.props.boxKey)
-    console.log(this.props.boxVal);
+    // console.log(this.props.boxVal);
     return (
       <button onClick={this.toggleState.bind(this)} className="button">
         {this.icon}
       </button>
     )  
   }
+
 }
 
 
